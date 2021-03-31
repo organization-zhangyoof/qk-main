e370a5e8fcafb2da3db7d9870de21231e80251ad
zhangyoof <Admin123>
2021-03-31 18:30:58 +0800

    提交

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 const home_route = require('./pages/home/routes');
 const route = [
     ...home_route,
-    {
-                 path: '/app1',
-                 microApp: 'app1',
-               },
+    // {
+    //              path: '/app1',
+    //              microApp: 'app1',
+    //            },
 ]
 const routes = [
     {
